34c0d8d9a2812120cbd65ccbf6a9e69fe20d8cde
黎酝 <2389231032@qq.com>
2022-03-22 21:18:22 +0800

    更新了

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 学习 Rust OS 中
+
+| Blink | Background | Foreground | Ascii |
+|  --   |     --     |     --     |  --   |
+|  15   |   12-14    |    8-11    |  0-7  |
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), "CYY What are you doing?").unwrap();
+    write!(vga_buffer::WRITER.lock(), "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n").unwrap();
+    
+    // panic!("Some panic message");
     loop {}
 }
+
+mod vga_buffer;
